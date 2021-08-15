5b49ea4d08f3d060c3e2de665c8d3098185fe7ef
Andi <andidev30.work@gmail.com>
2021-08-15 11:24:58 +0700

    add button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,28 @@
+import { Button as ButtonMui } from "@material-ui/core"
 import React from "react"
 
-const Button = (): JSX.Element => {
-  return <div>bro</div>
+type propsType = {
+  title: string
+}
+
+const Button = (props: propsType): JSX.Element => {
+  const { title } = props
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: 80,
+        margin: "0px auto",
+        left: 0,
+        right: 0,
+        textAlign: "center",
+      }}
+    >
+      <ButtonMui type="submit" variant="contained" disableElevation style={{ background: "#46C655", color: "#FFFFFF" }}>
+        {title}
+      </ButtonMui>
+    </div>
+  )
 }
 
 export default Button
